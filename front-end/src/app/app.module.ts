901ce06bde2c824e61d47fdbefea8a3e3de5621a
Lucas FRESIERS <fresiers.lucas@gmail.com>
2021-10-04 23:45:42 +0200

    Missing files & test checkbox

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,20 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
 import { MainComponent } from './main/main.component';
 import { UserListComponent } from './main/user-list/user-list.component';
 import { UserCardComponent } from './main/user-card/user-card.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './main/header/header.component';
+import { FiltersComponent } from './main/filters/filters.component';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 
-
-import { HeaderComponent } from './main/header/header.component';
-import { FiltersComponent } from './main/filters/filters.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -36,7 +36,9 @@ import { FiltersComponent } from './main/filters/filters.component';
     MatCardModule,
     MatButtonModule,
     InfiniteScrollModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/src/app/main/filters/filters.component.ts
+++ front-end/src/app/main/filters/filters.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
   styleUrls: ['./filters.component.less']
 })
+
 export class FiltersComponent implements OnInit {
 
-  constructor() { }
+  filters: FormGroup;
+
+  constructor(fb: FormBuilder) { 
+    this.filters = fb.group({
+      inTheStaff: false
+    });
+  }
+  
 
   ngOnInit(): void {
   }
